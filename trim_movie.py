72753f16f3ce85a146f3c3cdc84abdfd79c6d7eb
Peter Dobbs <dobbsp8@gmail.com>
2020-03-13 16:43:13 -0500

    Update trim_movie.py

--- trim_movie.py
+++ trim_movie.py
@@ -3,3 +3,9 @@ import sys
 
 video = VideoFileClip('vid/example.mp4').subclip(5, 10)
 video.write_videofile('vid/trimed.mp4', fps=30)
+
+# cropped = moviepy.video.fx.all.crop(video, x1=100, x2=950, y1=100, y2=900)
+# cropped.write_videofile('./video_output.mp4')
+
+# audio = video.audio
+# audio.write_audiofile('./audio_output.mp3')
