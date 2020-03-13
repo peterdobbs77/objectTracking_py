0ced49358f620d7e94e55d6e69681d4c5a9c49f3
Peter Dobbs <dobbsp8@gmail.com>
2020-03-13 17:27:25 -0500

    starting some work on pupil detection

---
+++ pupil_detection.py
@@ -0,0 +1,16 @@
+import cv2.cv2 as cv2
+
+
+video = cv2.VideoCapture('vid/eye.mp4')
+
+while True:
+    ok, frame = video.read()
+
+    if not ok:
+        break
+
+    cv2.imshow("Original", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
